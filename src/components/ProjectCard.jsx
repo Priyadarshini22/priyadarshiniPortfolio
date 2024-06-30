da94289ccfb6f555e1a936c0ccf7cfc2669911ac
Priyadarshini22 <61724074+Priyadarshini22@users.noreply.github.com>
2024-06-30 19:42:53 +0530

    image height changes

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,7 +3,7 @@ import { TbWorldWww } from "react-icons/tb";
 
 const ProjectCard = ({img,url,github,title,text}) => {
     return <article className="bg-white rounded-lg shadow-md hover:shadow-xl duration-300">
-    <img src={img} className='w-full object-cover rounded-t-lg h-15'/>
+    <img src={img} className='w-full object-cover rounded-t-lg' style={{height:'40%'}}/>
     <div className="capitalize p-8">
     <h2 className="text-xl tracking-wide font-medium">{title}</h2>
     <p className="mt-4 text-slate-700 leading-loose">{text}</p>
